002f8d63493de91e9c196d1046fd34db2b4d2d0d
rehemajaffar <rehemajumajaffar@gmail.com>
2022-04-01 23:24:25 +0300

    first commit

---
+++ src/main.kt
@@ -0,0 +1,20 @@
+fun main(){
+    char()
+    parameters()
+    interpret()
+}
+fun char(){
+    var text="Codehive"
+    var t = (text[0].toString()+text[1]+text[2]+text[3]+text[4]+text[5]+text[6]+text[7])
+    println(t)
+}
+fun parameters(){
+    var name="AkiraChix"
+    var time=5
+    var sentence=("Welcome in $name and it is  $time years since time of founded")
+    println(sentence)
+}
+fun interpret(){
+    var place ="Nairobi"
+    println(place.length)
+}
